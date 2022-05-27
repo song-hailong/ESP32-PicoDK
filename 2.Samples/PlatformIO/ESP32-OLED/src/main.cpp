04877d832cb92cc98cd3f937ba219d4287e4f780
宋海龙 <1298486187@qq.com>
2022-05-27 12:44:12 +0800

    修复OLED

--- 2.Samples/PlatformIO/ESP32-OLED/src/main.cpp
+++ 2.Samples/PlatformIO/ESP32-OLED/src/main.cpp
@@ -3,7 +3,7 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
-U8G2_SSD1306_128X80_NONAME_F_HW_I2C u8g2(U8G2_R1, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/21);
+U8G2_SH1107_128X80_F_HW_I2C u8g2(U8G2_R1, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/21);
 
 
 void setup(void)
